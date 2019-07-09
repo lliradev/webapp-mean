c0b66b56915b41afa08ba54ce29bf873f59fa1e1
LLira34 <luislira1029@gmail.com>
2019-07-08 19:56:38 -0500

    production

--- frontend/src/app/services/internal.service.ts
+++ frontend/src/app/services/internal.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Internal } from '../models/internal';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  URL_API: 'http://localhost:3000/api'
+  URL_API: '/api'
 };
