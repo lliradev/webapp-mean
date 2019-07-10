41e1cf895b0d260617343edea41c090723698441
LLira34 <luislira1029@gmail.com>
2019-07-10 12:56:35 -0500

    env

--- frontend/src/app/services/supplier.service.ts
+++ frontend/src/app/services/supplier.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Supplier } from '../models/supplier';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
